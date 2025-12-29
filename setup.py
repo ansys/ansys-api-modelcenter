fb9d0b903926d312142dcbade0a26a8b6e3f3570
Michel Pascale <35696270+phx-mpascale@users.noreply.github.com>
2025-12-29 14:01:45 -0500

    Maint/release 0.3.2 (#31)
    Co-authored-by: Mark Potter <56448836+potterphx@users.noreply.github.com>
    Co-authored-by: John Howard (JHH) <108291619+TheZaphod-Beeblebrox@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+# Copyright © 2024 ANSYS, Inc. Unauthorized use, distribution, or duplication is prohibited.
 """Installation file for the ansys-api-modelcenter package"""
 
 import os
@@ -39,8 +40,8 @@ if __name__ == "__main__":
         long_description_content_type="text/markdown",
         url=f"https://github.com/{username}/{package_name}",
         license="MIT",
-        python_requires=">=3.9,<4.0",
-        install_requires=["grpcio>=1.59", "protobuf>=3.19"],
+        python_requires=">=3.10,<4.0",
+        install_requires=["grpcio>=1.76", "protobuf==4.21.12"],
         packages=setuptools.find_namespace_packages(".", include=("ansys.*",)),
         package_data={
             "": ["*.proto", "*.pyi", "py.typed", "VERSION"],
